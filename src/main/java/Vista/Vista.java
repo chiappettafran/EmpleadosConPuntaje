cf1385ef58ac8b873b094da31302628262a98bea
AaronKibysz <aaronkibysz@gmail.com>
2024-06-18 15:30:12 -0300

    primer commit Vista

---
+++ src/main/java/Vista/Vista.java
@@ -0,0 +1,39 @@
+package Vista;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class Vista extends JFrame{
+
+    private JButton gestiondDeCursosButton;
+    private JButton gestionDeEmpleadosButton;
+    private JButton liquidacionDeSueldosButton;
+
+    public Vista(){
+        super("Gestion de Empleados y Cursos :3");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200,600);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 1));
+        panel.add(gestiondDeCursosButton);
+        panel.add(gestionDeEmpleadosButton);
+        panel.add(liquidacionDeSueldosButton);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public void agregarListenerGestionCursos(ActionListener listener){
+        gestiondDeCursosButton.addActionListener(listener);
+    }
+    public void agregarListenerGestionEmpleados(ActionListener listener){
+        gestionDeEmpleadosButton.addActionListener(listener);
+    }
+    public void agregarListenerLiquidacionSueldos(ActionListener listener){
+        liquidacionDeSueldosButton.addActionListener(listener);
+    }
+
+}
